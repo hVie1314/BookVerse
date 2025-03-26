0a29880ca423761b562b7f5dfba820f05977171e
hVie1314 <1212hoangviet@gmail.com>
2025-03-26 23:30:09 +0700

    fix: change connection string credentials

--- source/backend/src/configs/db/index.js
+++ source/backend/src/configs/db/index.js
@@ -1,14 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const dbURI = `mongodb+srv://${process.env.DB_USER}
-                           :${process.env.DB_PASSWORD}
-                           @${process.env.DB_HOST}
-                           /${process.env.DB_NAME}`;
+const CONNECTION_MONGODB_URL = process.env.CONNECTION_MONGODB_URL;
 
 async function connect() {
    try {
-      await mongoose.connect(dbURI);
+      await mongoose.connect(CONNECTION_MONGODB_URL);
       console.log("Connect to database successfully!!!");
    } catch (error) {
       console.log("Fail to connect to database!!!");
