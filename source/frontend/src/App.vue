c5036f0f071ae7b56ae8690010e6537a12dc3846
22120427 <nguyenmanhvan2k3@gmail.com>
2025-04-04 11:03:00 +0700

    fix: erro file create

--- source/frontend/src/App.vue
+++ source/frontend/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
@@ -24,3 +26,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
