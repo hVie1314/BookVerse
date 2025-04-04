5ea6d2487caa9ff66146a98d71cc5481a7ed1876
22120427 <nguyenmanhvan2k3@gmail.com>
2025-04-04 14:12:54 +0700

    chore: pull branch

---
+++ source/backend/src/configs/db/mongo.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const CONNECTION_MONGODB_URL = process.env.CONNECTION_MONGODB_URL;
+
+async function connect() {
+   try {
+      await mongoose.connect(CONNECTION_MONGODB_URL);
+      console.log("Connect to MongoDB successfully!!!");
+   } catch (error) {
+      console.log("Error connecting to MongoDB server!!!");
+   }
+}
+
+module.exports = { connect };
