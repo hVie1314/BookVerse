1bfb4b3f638e2a62007659a9184eca56456ed273
hVie1314 <1212hoangviet@gmail.com>
2025-04-08 22:20:44 +0700

    chore: enhance logout and token verification process in auth middleware

--- source/backend/src/app/middlewares/authMiddleware.js
+++ source/backend/src/app/middlewares/authMiddleware.js
@@ -1,30 +1,45 @@
 require("dotenv").config();
 const jwt = require('jsonwebtoken');
+const { redisClient } = require("../../configs/db/redis");
 
 class AuthMiddleware {
 
    verifyToken(req, res, next) {
-
-      const token = req.headers["authorization"].split(" ")[1];
+      const authHeader = req.headers["authorization"];
+      const token = authHeader && authHeader.split(" ")[1];
       if (!token) {
          return res.status(403).json({
             errorCode: "FORBIDDEN",
          });
       }
 
-      console.warn(token);
-
       // verify token
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
          if (err) {
             return res.status(401).json({
                errorCode: "UNAUTHORIZED",
             });
          }
 
-         req.userInfo = decoded;
+         try {
+            // get cached token from redis
+            const isInBlacklist = await redisClient.get(`blacklist:${token}`);
 
-         next();
+            if (isInBlacklist) {
+               return res.status(401).json({
+                  errorCode: "UNAUTHORIZED",
+               });
+            }
+
+            // passed all checks
+            req.userInfo = decoded;
+            next();
+            
+         } catch (error) {
+            return res.status(500).json({
+               errorCode: "INTERNAL_SERVER_ERROR",
+            });
+         }
       });
    }
 
