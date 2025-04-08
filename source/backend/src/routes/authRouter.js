9ccd4db2056116d1221b312acd8ccd36baef15f0
hVie1314 <1212hoangviet@gmail.com>
2025-04-08 21:47:03 +0700

    feat: update refresh token mechanism and add auth middleware for token verification

--- source/backend/src/routes/authRouter.js
+++ source/backend/src/routes/authRouter.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../app/controllers/authController');
+const authMiddleware = require('../app/middlewares/authMiddleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.post('/refresh-token', authController.refreshToken)
-router.post('/logout', authController.logout);
+router.get('/refresh-token', authMiddleware.verifyToken, authController.refreshToken)
+router.get('/logout', authMiddleware.verifyToken, authController.logout);
 
 module.exports = router;
