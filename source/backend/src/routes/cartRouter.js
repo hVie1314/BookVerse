ff0328d9c8d84ae1d1fd980a9d335e94b4c1f42e
hVie1314 <1212hoangviet@gmail.com>
2025-04-15 21:39:04 +0700

    feat: implement getCartByUserId functionality to retrieve user's cart details

--- source/backend/src/routes/cartRouter.js
+++ source/backend/src/routes/cartRouter.js
@@ -7,5 +7,6 @@ const authMiddleware = require('../app/middlewares/authMiddleware');
 router.post('/', cartController.addToCart);
 router.put('/', cartController.updateCart);
 router.delete('/', cartController.deleteProductInCart);
+router.get('/:userId', cartController.getCartByUserId);
 
 module.exports = router;
