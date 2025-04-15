7e62cb1086336e694a51f034a59a748dcce32d4f
hVie1314 <1212hoangviet@gmail.com>
2025-04-15 20:54:09 +0700

    feat: implement updateCart functionality to modify cart items and total price

--- source/backend/src/routes/cartRouter.js
+++ source/backend/src/routes/cartRouter.js
@@ -5,5 +5,6 @@ const cartController = require('../app/controllers/cartController');
 const authMiddleware = require('../app/middlewares/authMiddleware');
 
 router.post('/', cartController.addToCart);
+router.put('/', cartController.updateCart);
 
 module.exports = router;
