402cee981b99e670e96d8a2897e479475f23d2d5
hVie1314 <1212hoangviet@gmail.com>
2025-04-16 10:56:10 +0700

    feat: implement guest cart functionality and improve error handling in CartService

--- source/backend/src/routes/cartRouter.js
+++ source/backend/src/routes/cartRouter.js
@@ -4,9 +4,16 @@ const router = express.Router();
 const cartController = require('../app/controllers/cartController'); 
 const authMiddleware = require('../app/middlewares/authMiddleware');
 
+// user cart routes
 router.post('/', cartController.addToUserCart);
 router.put('/', cartController.updateUserCart);
 router.delete('/:userId', cartController.clearUserCart);
 router.get('/:userId', cartController.getUserCartByUserId);
+
+// guest cart routes
+router.post('/guest', cartController.addToGuestCart);
+router.put('/guest', cartController.updateGuestCart);
+router.delete('/guest/:cartId', cartController.clearGuestCart);
+router.get('/guest/:cartId', cartController.getGuestCartByCartId);
 
 module.exports = router;
