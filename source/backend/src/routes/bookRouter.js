e7acb81cc14d399409acd6f85b548f46556f9653
kieutrinh334 <duongngockieutrinh0303@gmail.com>
2025-04-17 21:43:31 +0700

    refactor: standardize endpoint for delete method to /book/:id in bookRouter

--- source/backend/src/routes/bookRouter.js
+++ source/backend/src/routes/bookRouter.js
@@ -6,7 +6,7 @@ const bookController = require('../app/controllers/bookController');
 // CRUD routes for books
 router.post('/', bookController.create);         // [POST] /book/
 router.put('/:id', bookController.update);       // [PUT] /book/:id
-router.delete('/books/:id', bookController.delete); // [DELETE] /books/:id
+router.delete('/:id', bookController.delete); // [DELETE] /books/:id
 router.get('/', bookController.getAll);          // [GET] /book/
 router.get('/:id', bookController.getById);      // [GET] /book/:id
 
