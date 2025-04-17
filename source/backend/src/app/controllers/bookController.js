8b5411ff2342327b81b443a072e509508e336f41
kieutrinh334 <duongngockieutrinh0303@gmail.com>
2025-04-17 21:29:06 +0700

    refactor: standardize responses in bookController

--- source/backend/src/app/controllers/bookController.js
+++ source/backend/src/app/controllers/bookController.js
@@ -3,19 +3,18 @@ const AppError = require('../../utils/appError');
 
 class BookController {
 
-   // [POST] /book/create
+   // [POST] /book/
    async create(req, res, next) {
       try {
          const newBook = new Book(req.body);
          const savedBook = await newBook.save();
-         return res.status(201).json({ success: true, data: savedBook });
+         return res.status(201).json({ book: savedBook });
       } catch (err) {
-         console.error('Create Book Error:', err);
          return next(new AppError(500, 'INTERNAL_SERVER_ERROR', err.message));
       }
    }
 
-   // [PUT] /book/update/:id
+   // [PUT] /book/:id
    async update(req, res, next) {
       try {
          const updatedBook = await Book.findByIdAndUpdate(
@@ -26,34 +25,31 @@ class BookController {
          if (!updatedBook) {
             return next(new AppError(404, 'NOT_FOUND'));
          }
-         return res.status(200).json({ success: true, data: updatedBook });
+         return res.status(200).json({ book: updatedBook });
       } catch (err) {
-         console.error('Update Book Error:', err);
          return next(new AppError(500, 'INTERNAL_SERVER_ERROR', err.message));
       }
    }
 
-   // [DELETE] /book/delete/:id
+   // [DELETE] /books/:id
    async delete(req, res, next) {
       try {
          const deletedBook = await Book.findByIdAndDelete(req.params.id);
          if (!deletedBook) {
             return next(new AppError(404, 'NOT_FOUND'));
          }
-         return res.status(200).json({ success: true, message: 'Book deleted successfully' });
+         return res.status(200).json({ message: 'Book deleted successfully' });
       } catch (err) {
-         console.error('Delete Book Error:', err);
          return next(new AppError(500, 'INTERNAL_SERVER_ERROR', err.message));
       }
    }
 
-   // [GET] /book
+   // [GET] /book/
    async getAll(req, res, next) {
       try {
          const books = await Book.find();
-         return res.status(200).json({ success: true, data: books });
+         return res.status(200).json({ books });
       } catch (err) {
-         console.error('Get All Books Error:', err);
          return next(new AppError(500, 'INTERNAL_SERVER_ERROR', err.message));
       }
    }
@@ -65,9 +61,8 @@ class BookController {
          if (!book) {
             return next(new AppError(404, 'NOT_FOUND'));
          }
-         return res.status(200).json({ success: true, data: book });
+         return res.status(200).json({ book });
       } catch (err) {
-         console.error('Get Book By ID Error:', err);
          return next(new AppError(500, 'INTERNAL_SERVER_ERROR', err.message));
       }
    }
