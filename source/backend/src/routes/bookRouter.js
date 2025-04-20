8faf04f1fa6cc4b52b4a8dbdbbc33562d4ce73b4
uyen312 <nguyenphamtuuyen0101@gmail.com>
2025-04-20 22:38:20 +0700

    fix: fix bookRouter.js

--- source/backend/src/routes/bookRouter.js
+++ source/backend/src/routes/bookRouter.js
@@ -5,12 +5,13 @@ const bookController = require('../app/controllers/bookController');
 
 router.get('/category', bookController.getCategory); 
 
+router.get('/search', bookController.search);   
+
 // CRUD routes for books
 router.post('/', bookController.create);         // [POST] /book/
 router.put('/:id', bookController.update);       // [PUT] /book/:id
 router.delete('/:id', bookController.delete); // [DELETE] /books/:id
 router.get('/', bookController.getAll);          // [GET] /book/
 router.get('/:id', bookController.getById);      // [GET] /book/:id
-router.get('/search', bookController.search);   // [GET] /book/search
 
 module.exports = router;
