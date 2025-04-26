643de62a0d2dca9292630d367e1d97ff252879a5
hVie1314 <1212hoangviet@gmail.com>
2025-04-26 12:39:06 +0700

    feat: implement Momo payment callback and transaction status check functionality

--- source/backend/src/utils/momo.js
+++ source/backend/src/utils/momo.js
@@ -1,4 +1,5 @@
 require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
+const AppError = require('./appError');
 const crypto = require('crypto');
 const axios = require('axios');
 
@@ -13,11 +14,11 @@ class Momo {
       this.requestType = 'payWithMethod';
       this.lang = 'vi';
       this.autoCapture = true;
-      this.momoUrl = process.env.MOMO_URL;
+      this.momoCreateUrl = process.env.MOMO_CREATE_URL;
+      this.momoQueryUrl = process.env.MOMO_QUERY_URL;
    }
 
-   async createPayment(amount, orderInfo, paymentCode) {
-      const orderId = this.partnerCode + new Date().getTime();
+   async createPayment(amount, orderInfo, orderId) {
       const requestId = orderId;
       const extraData = '';
       const orderGroupId = '';
@@ -46,19 +47,64 @@ class Momo {
       };
 
       try {
-         const response = await axios.post(this.momoUrl, requestBody, {
+         const response = await axios.post(this.momoCreateUrl, requestBody, {
             headers: {
                'Content-Type': 'application/json'
             }
          });
          
          //console.log(response.data);
-         return response.data.shortLink;
+         if(response.data.payUrl) {
+            return response.data.payUrl;
+         }
+         else {
+            console.log(response.data.message);
+            throw new AppError(400, "TRANSACTION_FAILED", response.data.message);
+         }
          
       } catch (error) {
-         throw new Error('500', "INTERNAL_SERVER_ERROR",error.message);
+         if (error instanceof AppError) throw error;
+         else throw new AppError(500, "INTERNAL_SERVER_ERROR", error.response.data.message);
       }
    }
+
+   async checkTransactionStatusByOrderId(orderId) {
+      try {
+         const rawSignature = `accessKey=${this.accessKey}&orderId=${orderId}&partnerCode=${this.partnerCode}&requestId=${orderId}`;
+         const signature = crypto
+           .createHmac('sha256', this.secretKey)
+           .update(rawSignature)
+           .digest('hex');
+       
+         const requestBody = JSON.stringify({
+           partnerCode: this.partnerCode,
+           requestId: orderId,
+           orderId: orderId,
+           signature: signature,
+           lang: this.lang,
+         });
+       
+         // options for axios
+         const options = {
+           method: 'POST',
+           url: this.momoQueryUrl,
+           headers: {
+             'Content-Type': 'application/json',
+           },
+           data: requestBody,
+         };
+       
+         const result = await axios(options);
+         //console.log(result.data);
+
+         return result.data.resultCode === 0 ? true : false;
+
+      } catch (error) {
+         if (error instanceof AppError) throw error;
+         else throw new AppError(500, "INTERNAL_SERVER_ERROR", error.message);
+      }
+   };
+
 }
 
 module.exports = new Momo();
