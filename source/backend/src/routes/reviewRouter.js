1ac05d466768ebaf568e9c3b1858868495043faf
kieutrinh334 <duongngockieutrinh0303@gmail.com>
2025-04-26 08:51:49 +0700

    refactor: Simplify review routes by using single review ID in URL

--- source/backend/src/routes/reviewRouter.js
+++ source/backend/src/routes/reviewRouter.js
@@ -4,11 +4,11 @@ const reviewController = require('../app/controllers/reviewController');
 const { verifyToken, verifyStaff } = require('../app/middlewares/authMiddleware');
 
 // CRUD routes for books
-router.post('/:bookId', reviewController.addReview);                                                // [POST] /review/
-router.put('/:bookId/:reviewId', reviewController.updateReview);                                    // [PUT] /review/:id
-router.delete('/:bookId/:reviewId', verifyToken, verifyStaff, reviewController.deleteReview);       // [DELETE] /review/:id
-router.get('/:bookId', reviewController.getAllReviews);                                             // [GET] /review/:bookId
-router.patch('/:bookId/:reviewId/hide', verifyToken, verifyStaff, reviewController.hideReview);     // [PATCH] /review/:id/hide
-router.patch('/:bookId/:reviewId/unhide', verifyToken, verifyStaff, reviewController.unhideReview); // [PATCH] /review/:id/unhide
+router.post('/:bookId', reviewController.addReview);                                  // [POST] /review/:bookId
+router.put('/:id', reviewController.updateReview);                                    // [PUT] /review/:id
+router.delete('/:id', verifyToken, verifyStaff, reviewController.deleteReview);       // [DELETE] /review/:id
+router.get('/:bookId', reviewController.getAllReviews);                               // [GET] /review/:bookId
+router.patch('/:id/hide', verifyToken, verifyStaff, reviewController.hideReview);     // [PATCH] /review/:id/hide
+router.patch('/:id/unhide', verifyToken, verifyStaff, reviewController.unhideReview); // [PATCH] /review/:id/unhide
 
 module.exports = router;
