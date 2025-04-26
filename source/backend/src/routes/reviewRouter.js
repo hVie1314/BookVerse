8fd7f1831912621a5035990b9843765d4cedf43a
kieutrinh334 <duongngockieutrinh0303@gmail.com>
2025-04-26 08:40:47 +0700

    feat: Enable hiding reviews via 'hidden' field

--- source/backend/src/routes/reviewRouter.js
+++ source/backend/src/routes/reviewRouter.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
 const reviewController = require('../app/controllers/reviewController');
+const { verifyToken, verifyStaff } = require('../app/middlewares/authMiddleware');
 
 // CRUD routes for books
-router.post('/:bookId', reviewController.addReview);                    // [POST] /review/
-router.put('/:bookId/:reviewId', reviewController.updateReview);        // [PUT] /review/:id
-router.delete('/:bookId/:reviewId', reviewController.deleteReview);     // [DELETE] /review/:id
-router.get('/:bookId', reviewController.getAllReviews);                 // [GET] /review/:bookId
+router.post('/:bookId', reviewController.addReview);                                                // [POST] /review/
+router.put('/:bookId/:reviewId', reviewController.updateReview);                                    // [PUT] /review/:id
+router.delete('/:bookId/:reviewId', verifyToken, verifyStaff, reviewController.deleteReview);       // [DELETE] /review/:id
+router.get('/:bookId', reviewController.getAllReviews);                                             // [GET] /review/:bookId
+router.patch('/:bookId/:reviewId/hide', verifyToken, verifyStaff, reviewController.hideReview);     // [PATCH] /review/hide
+router.patch('/:bookId/:reviewId/unhide', verifyToken, verifyStaff, reviewController.unhideReview);
 
 module.exports = router;
