9f42acca4c7463c758052382581b8a6c8df88a2f
hVie1314 <1212hoangviet@gmail.com>
2025-04-26 12:39:06 +0700

    feat: implement Momo payment callback and transaction status check functionality

--- source/backend/src/app/controllers/paymentController.js
+++ source/backend/src/app/controllers/paymentController.js
@@ -19,6 +19,36 @@ class PaymentController {
             next(error);
         }
     }
+
+    // [POST] /payment/momo/callback
+    async handleCallback(req, res, next) {
+        try {
+            //console.log(req.body);
+
+            const { orderId, resultCode } = req.body;
+
+            if (resultCode === 0) {
+                
+                // TODO: update order status in db
+                
+            }
+            
+        } catch (error) {
+            throw new AppError(500, "INTERNAL_SERVER_ERROR", error.message);
+        }
+    }
+
+    // [POST] /payment/momo/check-transaction-status
+    async checkTransactionStatus(req, res, next) {
+        try {
+            const { orderId } = req.body;
+            const transactionStatus = await momo.checkTransactionStatusByOrderId(orderId);
+
+            res.status(200).json({ status: transactionStatus });
+        } catch (error) {
+            next(error);
+        }
+    }
     
 }
 
--- source/backend/src/routes/paymentRouter.js
+++ source/backend/src/routes/paymentRouter.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const paymentController = require('../app/controllers/paymentController');
 
 router.post('/momo', paymentController.createPayment);
+router.post('/momo/callback', paymentController.handleCallback);
+router.post('/momo/check-transaction-status', paymentController.checkTransactionStatus);
 
 module.exports = router;
