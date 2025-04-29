659a763dbd28c898de149076c267cd60cc60f4ad
uyen312 <nguyenphamtuuyen0101@gmail.com>
2025-04-29 11:47:30 +0700

    fix: resolve bug in user controller and format user router

--- source/backend/src/app/controllers/userController.js
+++ source/backend/src/app/controllers/userController.js
@@ -6,27 +6,27 @@ class UserController {
     async getAllCustomer(req, res, next) {        
         try {
             const customers = await user.find({ role: 'user' });
-            res.status(200).json(customers);
+            return res.status(200).json(customers);
         } catch (err) {
-            next(new AppError(500, 'INTERNAL_SERVER_ERROR'));
+            return next(new AppError(500, 'INTERNAL_SERVER_ERROR'));
         }
     }
 
     async getAllStaff(req, res, next) {        
         try {
             const staffs = await user.find({ role: 'staff' });
-            res.status(200).json(staffs);
+            return res.status(200).json(staffs);
         } catch (err) {
-            next(new AppError(500, 'INTERNAL_SERVER_ERROR'));
+            return next(new AppError(500, 'INTERNAL_SERVER_ERROR'));
         }
     }
 
     async getAllAdmin(req, res, next) {        
         try {
             const admins = await user.find({ role: 'admin' });
-            res.status(200).json(admins);
+            return res.status(200).json(admins);
         } catch (err) {
-            next(new AppError(500, 'INTERNAL_SERVER_ERROR'));
+            return next(new AppError(500, 'INTERNAL_SERVER_ERROR'));
         }
     }
 
@@ -37,7 +37,7 @@ class UserController {
                 return next(new AppError(404, 'NOT_FOUND', 'User not found'));
             res.status(200).json(foundUser);
         } catch (err) {
-            next(new AppError(400, 'INVALID_USER_ID'));
+            return next(new AppError(400, 'INVALID_USER_ID'));
         }
     }
 
@@ -70,7 +70,7 @@ class UserController {
                 return next(new AppError(404, 'NOT_FOUND', 'User not found'));
             res.status(200).json(updatedUser);
         } catch (err) {
-            next(new AppError(400, "INVALID_UPDATE"));
+            return next(new AppError(400, "INVALID_UPDATE"));
         }
     }
 
@@ -81,7 +81,7 @@ class UserController {
                 return next(new AppError(404, 'NOT_FOUND', 'User not found'));
             res.status(204).json();
         } catch (err) {
-            next(new AppError(400, "DELETE_FAILED"));
+            return next(new AppError(400, "DELETE_FAILED"));
         }
     }
 }
