93e9ecf1043d3a5e896576f55f44b7a30bd2951b
hVie1314 <1212hoangviet@gmail.com>
2025-05-06 21:32:30 +0700

    chore: change fields in Review and check conditions before review

--- source/backend/src/app/models/Review.js
+++ source/backend/src/app/models/Review.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const ReviewSchema = new mongoose.Schema({
-    user_id: {
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
-    book_id: {
+    bookId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Book',
         required: true
