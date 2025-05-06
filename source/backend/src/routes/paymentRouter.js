cf3f51395aa5d51de2892c0ae61206de17616ebe
hVie1314 <1212hoangviet@gmail.com>
2025-05-06 23:17:22 +0700

    feat: enhance route security by adding authentication middleware to various routes

--- source/backend/src/routes/paymentRouter.js
+++ source/backend/src/routes/paymentRouter.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const paymentController = require('../app/controllers/paymentController');
+const authMiddleware = require('../app/middlewares/authMiddleware');
 
-router.post('/momo', paymentController.createPayment);
+router.post('/momo', authMiddleware.verifyToken, authMiddleware.verifyUser, paymentController.createPayment);
+router.post('/momo/check-transaction-status', authMiddleware.verifyToken, authMiddleware.verifyUserOrAdmin,  paymentController.checkTransactionStatus);
 router.post('/momo/callback', paymentController.handleCallback);
-router.post('/momo/check-transaction-status', paymentController.checkTransactionStatus);
 
 module.exports = router;
