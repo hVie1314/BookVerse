4194c062d29222fb88ba652dc684263c6e377b21
hVie1314 <1212hoangviet@gmail.com>
2025-05-08 13:32:50 +0700

    fix: update user ID parameter from ':id' to ':userId' and eliminate password field from responses

--- source/backend/src/app/controllers/userController.js
+++ source/backend/src/app/controllers/userController.js
@@ -6,6 +6,12 @@ class UserController {
     async getAllCustomer(req, res, next) {        
         try {
             const customers = await user.find({ role: 'user' });
+
+            // eliminate password field from the response
+            customers.forEach(customer => {
+                customer.password = null;
+            });
+
             return res.status(200).json(customers);
         } catch (err) {
             return next(new AppError(500, 'INTERNAL_SERVER_ERROR'));
@@ -15,6 +21,12 @@ class UserController {
     async getAllStaff(req, res, next) {        
         try {
             const staffs = await user.find({ role: 'staff' });
+
+            // eliminate password field from the response
+            staffs.forEach(staff => {
+                staff.password = null;
+            });
+
             return res.status(200).json(staffs);
         } catch (err) {
             return next(new AppError(500, 'INTERNAL_SERVER_ERROR'));
@@ -24,6 +36,12 @@ class UserController {
     async getAllAdmin(req, res, next) {        
         try {
             const admins = await user.find({ role: 'admin' });
+
+            // eliminate password field from the response
+            admins.forEach(admin => {
+                admin.password = null;
+            });
+
             return res.status(200).json(admins);
         } catch (err) {
             return next(new AppError(500, 'INTERNAL_SERVER_ERROR'));
@@ -32,9 +50,13 @@ class UserController {
 
     async getUserById(req, res, next) {
         try {
-            const foundUser = await user.findById(req.params.id);
+            const foundUser = await user.findById(req.params.userId);
             if (!foundUser)
                 return next(new AppError(404, 'NOT_FOUND', 'User not found'));
+
+            // eliminate password field from the response
+            foundUser.password = null;
+
             res.status(200).json(foundUser);
         } catch (err) {
             return next(new AppError(400, 'INVALID_USER_ID'));
@@ -61,7 +83,7 @@ class UserController {
             }
 
             // Find the user by ID and update the allowed fields
-            const updatedUser = await user.findByIdAndUpdate(req.params.id, updatedData, {
+            const updatedUser = await user.findByIdAndUpdate(req.params.userId, updatedData, {
                 new: true,             
                 runValidators: true  
             });
@@ -76,7 +98,7 @@ class UserController {
 
     async deleteUser(req, res, next) {
         try {
-            const deleted = await user.findByIdAndDelete(req.params.id);
+            const deleted = await user.findByIdAndDelete(req.params.userId);
             if (!deleted) 
                 return next(new AppError(404, 'NOT_FOUND', 'User not found'));
             res.status(204).json();
