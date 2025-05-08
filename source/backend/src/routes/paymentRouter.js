0de991282bb965c6ab78f81973c94c440a50d81a
hVie1314 <1212hoangviet@gmail.com>
2025-05-08 16:11:15 +0700

    fix: update review routes to use userId parameter for consistency

--- source/backend/src/routes/paymentRouter.js
+++ source/backend/src/routes/paymentRouter.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const paymentController = require('../app/controllers/paymentController');
 const authMiddleware = require('../app/middlewares/authMiddleware');
 
+router.post('/momo/callback', paymentController.handleCallback);
 router.post('/momo/:userId', authMiddleware.verifyToken, authMiddleware.verifyUser, paymentController.createPayment);
 router.post('/momo/check-transaction-status/:userId', authMiddleware.verifyToken, authMiddleware.verifyUserOrAdmin,  paymentController.checkTransactionStatus);
-router.post('/momo/callback', paymentController.handleCallback);
 
 module.exports = router;
