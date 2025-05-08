afd4fd8e5115bb7354b539e6ba590fb70a5cd73b
hVie1314 <1212hoangviet@gmail.com>
2025-05-08 14:35:19 +0700

    fix: update payment routes to include userId parameter for clarity

--- source/backend/src/routes/paymentRouter.js
+++ source/backend/src/routes/paymentRouter.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const paymentController = require('../app/controllers/paymentController');
 const authMiddleware = require('../app/middlewares/authMiddleware');
 
-router.post('/momo', authMiddleware.verifyToken, authMiddleware.verifyUser, paymentController.createPayment);
-router.post('/momo/check-transaction-status', authMiddleware.verifyToken, authMiddleware.verifyUserOrAdmin,  paymentController.checkTransactionStatus);
+router.post('/momo/:userId', authMiddleware.verifyToken, authMiddleware.verifyUser, paymentController.createPayment);
+router.post('/momo/check-transaction-status/:userId', authMiddleware.verifyToken, authMiddleware.verifyUserOrAdmin,  paymentController.checkTransactionStatus);
 router.post('/momo/callback', paymentController.handleCallback);
 
 module.exports = router;
