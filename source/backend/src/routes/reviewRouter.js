799e5d9d43f36b4ca05e729db1490c8c051d59f6
hVie1314 <1212hoangviet@gmail.com>
2025-05-08 17:20:21 +0700

    fix: update review and book service routes to use correct parameter names

--- source/backend/src/routes/reviewRouter.js
+++ source/backend/src/routes/reviewRouter.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const reviewController = require('../app/controllers/reviewController');
 const authMiddleware = require('../app/middlewares/authMiddleware');
 
-router.post('/:userId',authMiddleware.verifyToken, authMiddleware.verifyUser, reviewController.addReview);                    
-router.put('/:userId', authMiddleware.verifyToken, authMiddleware.verifyUser, reviewController.updateReview);                      
-router.delete('/:userId', authMiddleware.verifyToken, authMiddleware.verifyUserOrStaffOrAdmin, reviewController.deleteReview);      
+router.post('/:bookId',authMiddleware.verifyToken, authMiddleware.verifyUser, reviewController.addReview);                    
+router.put('/:reviewId', authMiddleware.verifyToken, authMiddleware.verifyUser, reviewController.updateReview);                      
+router.delete('/:reviewId', authMiddleware.verifyToken, authMiddleware.verifyUserOrStaffOrAdmin, reviewController.deleteReview);      
 router.get('/:bookId', reviewController.getAllReviews);                               
 router.patch('/hide/:id', authMiddleware.verifyToken, authMiddleware.verifyStaffOrAdmin, reviewController.hideReview);    
 router.patch('/unhide/:id', authMiddleware.verifyToken, authMiddleware.verifyStaffOrAdmin, reviewController.unhideReview); 
