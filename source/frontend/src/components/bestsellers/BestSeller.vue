395ce93e694b838e35fc165ba74d39096c9c7eae
uyen312 <nguyenphamtuuyen0101@gmail.com>
2025-06-13 17:00:05 +0700

    style: reformat bestsellers component

--- source/frontend/src/components/bestsellers/BestSeller.vue
+++ source/frontend/src/components/bestsellers/BestSeller.vue
@@ -15,7 +15,7 @@
                 @click="previousPage" 
                 class="carousel-control-prev"
                 type="button"
-                >
+            >
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
@@ -47,7 +47,7 @@
                 @click="nextPage" 
                 class="carousel-control-next"
                 type="button"
-                >
+            >
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
